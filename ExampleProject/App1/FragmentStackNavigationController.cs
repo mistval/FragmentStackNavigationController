a0f92058e8dcbd3752838e00497adfb7f87a6431
Randall <randallschmidt1@gmail.com>
2019-02-10 17:28:11 +0700

    Update FragmentStackNavigationController.cs

--- ExampleProject/App1/FragmentStackNavigationController.cs
+++ ExampleProject/App1/FragmentStackNavigationController.cs
@@ -1,7 +1,7 @@
 /*
 * MIT License
 *
-* Copyright (c) 2019 mistval
+* Copyright (c) 2019 Randall
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy
 * of this software and associated documentation files (the "Software"), to deal
